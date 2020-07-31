e6bf51e9039640b253d2c433661edd000781dc6c
姚季 <1543544497@qq.com>
2020-07-31 09:40:56 +0800

    SSM案例

--- myoffice/src/main/java/com/neuedu/myoffice/controller/DepartmentController.java
+++ myoffice/src/main/java/com/neuedu/myoffice/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package com.neuedu.myoffice.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neuedu.myoffice.bean.ResultBean;
 import com.neuedu.myoffice.entity.Department;
@@ -33,4 +37,27 @@ public class DepartmentController {
 		return "department/managerDepartments";
 	}
 	
+	@RequestMapping(value = "department/createDepartment",method = RequestMethod.GET)
+	public String x() {
+		return "department/createDepartment";
+	}
+	
+	@RequestMapping(value = "",method = RequestMethod.POST)
+	@ResponseBody
+	public ResultBean<String> insert(String id, String name, String type, String telephone, String fax, String description, String predepartment, String date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Department department = new Department(id, name, type, telephone, fax, description, predepartment,sdf.parse(date));
+		boolean flag = departmentService.insert(department);
+		ResultBean<String> resultBean = new ResultBean<String>();
+		if (flag) {
+			resultBean.setCode(200);
+			resultBean.setSuccessed(true);
+			resultBean.setMessage("新增部门成功");
+		}else {
+			resultBean.setCode(500);
+			resultBean.setSuccessed(false);
+			resultBean.setMessage("删除部门成功");
+		}
+		return resultBean;
+	}
 }
